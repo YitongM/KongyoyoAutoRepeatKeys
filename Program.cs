7bea82423fe057ad1e2560e618da1793e9cdba91
Yitong <yitong.mao@hotmail.fr>
2022-04-21 15:31:19 +0200

    Init Existing version on pc

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+namespace AutoRepeat
+{
+    internal class Program
+    {
+        [DllImport("USER32.DLL")]
+        public static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        private static void Main(string[] args)
+        {
+            //Checks if application is running
+            try
+            {
+                Process process = Process.GetProcessesByName("MemoQ")[0];
+                if (process != null)
+                {
+                    process.WaitForInputIdle();
+                    IntPtr s = process.MainWindowHandle;
+                    SetForegroundWindow(s);
+
+                    SendKeys.SendWait("^{ENTER 260}");
+                    Console.WriteLine("Amazingly done like a pro!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("ERROR: Application stopped running!\nException: \n" + ex);
+                Console.ReadKey();
+                return;
+            }
+        }
+    }
+}
